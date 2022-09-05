cc18cfca443d54789d72eb7c730b000d746ae48a
braxvalle <braxvalle2@gmail.com>
2022-09-05 19:05:49 +0300

    javascript warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv.slice(2).length > 1) {
   console.log('Arguments found');
-} else if (process.argv.slice(2).length == 1) {
+} else if (process.argv.slice(2).length === 1) {
   console.log('Argument found');
 } else {
   console.log('No argument');

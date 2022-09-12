18c48a307cd31ce176ccc688a56c58b3f94c87ea
unknown <braxvalle>
2022-09-12 19:32:31 +0300

    object relational

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""
-script that lists all states with a name starting /
-with N (upper N) from the database hbtn_0e_0_usa
-"""
+"""lists all states from the database hbtn_0e_0_usa"""
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     import MySQLdb
-    from sys import argv
+    import sys
 
-    db_conn = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-    cursor = db_conn.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
-    db_conn.close()
     
